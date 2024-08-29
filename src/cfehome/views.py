1c44ba8d53dbda7f1c879a5273e8d798f96fe8e8
jkracke96 <80530341+jkracke96@users.noreply.github.com>
2024-08-29 15:35:16 +0200

    updated views

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -10,12 +10,12 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisits.objects.all()
     page_qs = PageVisits.objects.filter(path=request.path)
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "total_visit_count": qs.count()
     }
-    html_template = "home.html"
     PageVisits.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
