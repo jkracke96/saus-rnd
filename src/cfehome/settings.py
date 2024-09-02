d67f954609d91715f4df240332ee46ccee674b64
jkracke96 <80530341+jkracke96@users.noreply.github.com>
2024-09-02 15:46:57 +0200

    getting started with tailwind and static files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -139,6 +139,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+# source for manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# output for manage.py collectstatic
+# local/prod CDN
+STATIC_ROOT = BASE_DIR / 'local-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
