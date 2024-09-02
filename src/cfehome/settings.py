5abd6899ac00601a3b4d66c042c52a56ce6ba112
jkracke96 <80530341+jkracke96@users.noreply.github.com>
2024-09-02 13:46:45 +0200

    introduce envrironment variables

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hqbz54^31x*=%eisujraibpkb$%5@#j5s8iux@$&6!v3=8=@&i'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')).lower() == 'true'
-print('DEBUG', DEBUG, type(DEBUG))
+#DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true'
+DEBUG = config('DJANGO_DEBUG', cast=bool)
+print("DJANGO_DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     '.railway.app'
