d911a925a771733e1de439d723a43b321897445c
jkracke96 <80530341+jkracke96@users.noreply.github.com>
2025-03-08 14:57:51 +0100

    update base url for production

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -37,9 +37,6 @@ MANAGERS = ADMINS
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool)
 BASE_URL = config('BASE_URL', default=None)
-if not BASE_URL:
-    print(config('RAILWAY_PUBLIC_DOMAIN', default=None))
-    BASE_URL = 'https://' + config('RAILWAY_PUBLIC_DOMAIN', default=None)
 
 ALLOWED_HOSTS = [
     '.railway.app',
